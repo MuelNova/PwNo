3a12f0fd99dd7478bc5b3cbba4bd4cdf4aaec3b0
MuelNova <n@ova.moe>
2024-09-22 16:32:20 +0800

    :children_crossing: Command arg will overwrite args from \`gen_sh\`

--- src/pwno/context/gen_sh.py
+++ src/pwno/context/gen_sh.py
@@ -15,27 +15,28 @@ def gen_sh(*a, f_or_h: str | Path = None, port: int = None, **kw) -> process | r
     如果传入的参数有两个，则会将第一个参数作为 config.HOST，第二个参数作为 config.PORT。
 
     """
-    if len(a) == 1 or (f_or_h is not None and port is None):
-        s = f_or_h or a[0]
-        if ":" in s and len(r := s.split(":")) == 2 and r[1].isdigit():
-            config.HOST = r[0]
-            config.PORT = int([1])
+    if not config.REMOTE and not config.GDB and not config.ATTACHMENT:
+        if len(a) == 1 or (f_or_h is not None and port is None):
+            s = f_or_h or a[0]
+            if ":" in s and len(r := s.split(":")) == 2 and r[1].isdigit():
+                config.HOST = r[0]
+                config.PORT = int(r[1])
+                config.REMOTE = True
+            else:
+                config.ATTACHMENT = s if isinstance(s, str) else str(s)
+
+            if len(a) >= 1:
+                a = a[1:]
+
+        elif len(a) == 2 or (f_or_h is not None and port is not None):
+            s = f_or_h or a[0]
+            p = port or a[1]
+            config.HOST = s
+            config.PORT = p
             config.REMOTE = True
-        else:
-            config.ATTACHMENT = s if isinstance(s, str) else str(s)
 
-        if len(a) >= 1:
-            a = a[1:]
-
-    elif len(a) == 2 or (f_or_h is not None and port is not None):
-        s = f_or_h or a[0]
-        p = port or a[1]
-        config.HOST = s
-        config.PORT = p
-        config.REMOTE = True
-
-        if len(a) >= 2:
-            a = a[2:]
+            if len(a) >= 2:
+                a = a[2:]
 
     if config.REMOTE:
         return remote(config.HOST, config.PORT)
